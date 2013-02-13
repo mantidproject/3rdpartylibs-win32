f6fa3f8afd4a6138789765d71132295f356074d3
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-13 11:46:22 +0000

    Removed unnecessary .pyc,.pyo files Refs #6527

--- Python27/Scripts/easy_install-script.py
+++ Python27/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!python.exe
+#!c:\Users\dmn58364\Code\mantid\Code\Third_Party\lib\win32\Python27\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.6c11','console_scripts','easy_install'
 __requires__ = 'setuptools==0.6c11'
 import sys
